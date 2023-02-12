a43a51c51bfee29c5eb6f2f69d72f7a58876512f
LeUmbono <92232892+LeUmbono@users.noreply.github.com>
2023-02-11 17:10:54 -0800

    test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React from "react";
 import Dashboard from "./component/DashBoard";
-import Form from './component/Form';
-import Courses from './component/Form';
+import Form from "./component/Form";
+import Courses from "./component/Form";
+import ResumePDF from "./component/ResumePDF";
 
 const App = () => {
-  return <Dashboard />
+  return <ResumePDF />;
 };
 
 export default App;
