4c081a76be98f25c495a7ad3f3a59fcec85306e7
shijie <shijiechen555@gmail.com>
2023-02-11 20:14:25 -0800

    updated skill and work experience section

--- src/component/Form/NavBar.js
+++ src/component/Form/NavBar.js
@@ -1,9 +1,9 @@
-import { AppBar, IconButton, Toolbar, Typography } from "@mui/material";
+import { AppBar, IconButton, Toolbar, } from "@mui/material";
 import { React } from "react";
 import MenuIcon from "@mui/icons-material/Menu";
 import { useNavigate } from "react-router-dom";
 import Button from '@mui/material/Button';
-import { createMuiTheme } from "@material-ui/core/styles";  
+// import { createMuiTheme } from "@material-ui/core/styles";  
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 const theme = createTheme({
