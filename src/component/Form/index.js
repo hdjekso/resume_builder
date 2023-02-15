3909e022fb19f113e6d388fe7f75ea26cf57d386
LeUmbono <92232892+LeUmbono@users.noreply.github.com>
2023-02-15 13:42:34 -0800

    set up download button and placeholder pdf

--- src/component/Form/index.js
+++ src/component/Form/index.js
@@ -75,30 +75,6 @@ const Form = () => {
     handleNext();
   };
 
-  const docDefinition = {
-    content: [
-      // if you don't need styles, you can use a simple string to define a paragraph
-      "This is a standard paragraph, using default style",
-
-      // using a { text: '...' } object lets you set styling properties
-      { text: "This paragraph will have a bigger font", fontSize: 15 },
-
-      // if you set the value of text to an array instead of a string, you'll be able
-      // to style any part individually
-      {
-        text: [
-          "This paragraph is defined as an array of elements to make it possible to ",
-          { text: "restyle part of it and make it bigger ", fontSize: 15 },
-          "than the rest.",
-        ],
-      },
-    ],
-  };
-  const handleDownloadResume = () => {
-    const pdfGenerator = pdfMake.createPdf(docDefinition);
-    pdfGenerator.download();
-  };
-
   const handleReset = () => {
     setActiveStep(0);
     setCompleted({});
@@ -169,9 +145,7 @@ const Form = () => {
           </div>
         </Box>
       </Container>
-      <div>
-        <Button onClick={handleDownloadResume}>Hello</Button>
-      </div>
+      <ResumePDF></ResumePDF>
     </React.Fragment>
   );
 };
--- src/component/ResumePDF.js
+++ src/component/ResumePDF.js
@@ -1,29 +1,39 @@
 import React from "react";
+import { Button } from "@mui/material";
 import pdfMake from "pdfmake/build/pdfmake";
 import pdfFonts from "pdfmake/build/vfs_fonts";
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
-const docDefinition = {
-  content: [
-    // if you don't need styles, you can use a simple string to define a paragraph
-    "This is a standard paragraph, using default style",
+const ResumePDF = () => {
+  const docDefinition = {
+    content: [
+      // if you don't need styles, you can use a simple string to define a paragraph
+      "This is a standard paragraph, using default style",
 
-    // using a { text: '...' } object lets you set styling properties
-    { text: "This paragraph will have a bigger font", fontSize: 15 },
+      // using a { text: '...' } object lets you set styling properties
+      { text: "This paragraph will have a bigger font", fontSize: 15 },
 
-    // if you set the value of text to an array instead of a string, you'll be able
-    // to style any part individually
-    {
-      text: [
-        "This paragraph is defined as an array of elements to make it possible to ",
-        { text: "restyle part of it and make it bigger ", fontSize: 15 },
-        "than the rest.",
-      ],
-    },
-  ],
+      // if you set the value of text to an array instead of a string, you'll be able
+      // to style any part individually
+      {
+        text: [
+          "This paragraph is defined as an array of elements to make it possible to ",
+          { text: "restyle part of it and make it bigger ", fontSize: 15 },
+          "than the rest.",
+        ],
+      },
+    ],
+  };
+
+  const handleDownloadResume = () => {
+    const pdfGenerator = pdfMake.createPdf(docDefinition);
+    pdfGenerator.download();
+  };
+  return (
+    <div>
+      <Button onClick={handleDownloadResume}>Download Resume</Button>
+    </div>
+  );
 };
 
-const handleDownloadResume = () => {
-  const pdfGenerator = pdfMake.createPdf(docDefinition);
-  pdfGenerator.download();
-};
+export default ResumePDF;
