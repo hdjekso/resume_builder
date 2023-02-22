d3aab31eae122f7be7bc00f7a0a07d6b3e4adf3d
LeUmbono <92232892+LeUmbono@users.noreply.github.com>
2023-02-21 16:48:48 -0800

    finished rough look of template up until experience

--- src/component/ResumePDF.js
+++ src/component/ResumePDF.js
@@ -5,24 +5,110 @@ import pdfFonts from "pdfmake/build/vfs_fonts";
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 const ResumePDF = () => {
+  var expData = {
+    jobTitle: "JOB TITLE",
+    jobDate: "JOB DATE",
+    jobOrg: "COMPANY",
+    jobLocation: "CITY, STATE",
+    jobDetails: ["BP 1", "BP 2", "BP 3"],
+  };
+  var experienceData = [];
+
+  function addExperience(data) {
+    experienceData.push({
+      columns: [
+        {
+          text: data.jobTitle + "\n" + data.jobOrg + "\n",
+          width: "*",
+          alignment: "left",
+        },
+        {
+          text: data.jobDate + "\n" + data.jobLocation + "\n",
+          width: "*",
+          alignment: "right",
+        },
+      ],
+    });
+    var detailsArray = [];
+    data.jobDetails.forEach((element) => {
+      detailsArray.push(element);
+    });
+
+    experienceData.push({ ul: [{ ul: detailsArray }] });
+    experienceData.push({ text: "\n" });
+  }
+
+  addExperience(expData);
+  addExperience(expData);
+
   const docDefinition = {
     content: [
       // if you don't need styles, you can use a simple string to define a paragraph
-      "This is a standard paragraph, using default style",
+      { text: "FIRSTNAME MIDDLENAME LASTNAME", style: "name" },
 
       // using a { text: '...' } object lets you set styling properties
-      { text: "This paragraph will have a bigger font", fontSize: 15 },
+      { text: "PHONE NUM | ADDRESS", alignment: "center", style: "body" },
 
-      // if you set the value of text to an array instead of a string, you'll be able
-      // to style any part individually
       {
-        text: [
-          "This paragraph is defined as an array of elements to make it possible to ",
-          { text: "restyle part of it and make it bigger ", fontSize: 15 },
-          "than the rest.",
-        ],
+        text: "LINK1 | LINK2 | LINK3",
+        alignment: "center",
+        style: "body",
       },
+
+      {
+        text: "\nEDUCATION",
+        style: "header",
+      },
+
+      {
+        columns: [
+          {
+            text: [
+              { text: "DEGREE NAME", bold: true },
+              { text: " , " },
+              { text: "UNIVERSITY" },
+            ],
+            width: "*",
+            alignment: "left",
+          },
+          { text: "DATE", width: "*", alignment: "right" },
+        ],
+        style: "body",
+      },
+
+      {
+        text: "\nSKILLS",
+        style: "header",
+      },
+
+      {
+        text: [{ text: "Technical Skills\t" }, { text: "A B C D" }],
+        style: "body",
+      },
+
+      {
+        text: "\nEXPERIENCE",
+        style: "header",
+      },
+      experienceData,
+
+      //{ columns: [], style: "body" },
     ],
+    styles: {
+      name: {
+        fontSize: 18,
+        bold: true,
+        alignment: "center",
+      },
+      header: {
+        fontsize: 14,
+        bold: true,
+        decoration: "underline",
+      },
+      body: {
+        fontSize: 12,
+      },
+    },
   };
 
   const handleDownloadResume = () => {
