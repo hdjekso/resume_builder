d495b8facc48878bb72b2b8451491c681eb78bd4
Steven Gong <120246964+Roszillary@users.noreply.github.com>
2023-02-27 19:30:35 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
+/*
 import Dashboard from "./component/DashBoard";
 import Form from "./component/Form";
 import Courses from "./component/Form";
+*/
 import ResumePDF from "./component/ResumePDF";
-import { PDFDownloadLink } from "@react-pdf/renderer";
+//import { PDFDownloadLink } from "@react-pdf/renderer";
+
 
 const App = () => {
-  return <Dashboard />;
+  return <ResumePDF />;
 };
 
 export default App;
