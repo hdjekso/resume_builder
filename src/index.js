a258dfba53c289bb3d16f50a0284027529cdccfa
Steven Gong <120246964+Roszillary@users.noreply.github.com>
2023-02-27 19:30:41 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,29 +1,20 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import  ReactDOM from "react-dom/client";
 import App from "./App";
-import { Provider } from "react-redux";
-import { store } from "./action/index";
 // s
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Form from "./component/Form";
+//import Form from "./component/Form";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
+ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
-      {/* <BrowserRouter> stores the current location in the brower's addres bar 
+    {/* <BrowserRouter> stores the current location in the brower's addres bar 
     using clean URLs and navigates using the brower'built-in history stack */}
-      <Routes>
-        <Route
-          path="/"
-          element={
-            //   <Provider store={store}>
-            <App />
-            //   </Provider>
-          }
-        />
-        <Route path="/form" element={<Form />} />
-      </Routes>
+        <Routes>
+            <Route path='/' element={<App />} />
+            <Route /*path='/form' element={<Form />}*/ />
+        </Routes>
     </BrowserRouter>
-  </Provider>
 );
+
+
